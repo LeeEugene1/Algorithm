f27aafa8d3d4039d4b649eea6bf1fb3318741a86
LeeEugene1 <uwm1004@gmail.com>
2023-03-09 21:37:17 +0900

    feat: 프로그래머스 5일차

---
+++ 프로그래머스/5.같은숫자는싫어.js
@@ -0,0 +1,13 @@
+function solution(arr){
+    answer = []
+    answer.push(arr[0])
+    for(let i = 0; i<arr.length; i++){
+        if(arr[i] === answer[answer.length - 1]){
+            answer.push(arr[i])
+            answer.pop()
+        }else{
+            answer.push(arr[i])
+        }
+    }
+    return answer
+}
