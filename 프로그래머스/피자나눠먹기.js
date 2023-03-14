bde79021e49043b81e8c3b72c434ac984b8af546
LeeEugene1 <uwm1004@gmail.com>
2023-03-14 18:27:44 +0900

    피자나눠먹기2

--- 프로그래머스/피자나눠먹기.js
+++ 프로그래머스/피자나눠먹기.js
@@ -2,3 +2,19 @@
 function solution(n) {
     return Math.ceil(n/7);
 }
+
+//피자나눠먹기2
+function solution(n) {
+    // 피자 6조각 모드같은양을먹는다
+    // 10명 => 6조각*?/10명 = 3조각씩
+    // ?는 5
+    var answer = 1;
+    while(true){
+       if(6*answer%n === 0){
+           break;
+       } 
+       answer++
+    }
+    
+    return answer;
+}
